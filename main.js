83d8f9445862d89237a0ebfc2c9cb71607a50818
CarlosRuiz <ingecarlosrm@gmail.com>
2024-01-16 23:12:39 -0600

    [UPDATE] email v3

--- main.js
+++ main.js
@@ -2,16 +2,25 @@ document.getElementById('searchButton').addEventListener('click', function() {
     var userEmail = document.getElementById('userEmail').value;
     
     if (userEmail) {
-        fetch('https://hooks.zapier.com/hooks/catch/12589050/3wnyhbi/', {
+        // Asegúrate de reemplazar esta URL con la URL de tu webhook de Zapier
+        var zapierUrl = 'https://hooks.zapier.com/hooks/catch/12589050/3wnyhbi/';
+
+        fetch(zapierUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({ email: userEmail })
         })
-        .then(response => response.text()) // Asumiendo que la respuesta es texto
-        .then(result => {
-            document.getElementById('result').innerHTML = 'Datos encontrados: ' + result;
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // o response.text() si la respuesta es texto
+        })
+        .then(data => {
+            // Aquí manejas los datos recibidos. Ajusta según la estructura de tu respuesta.
+            document.getElementById('result').innerHTML = 'Nombre: ' + data.Nombre + ', Total Puntos: ' + data.totalPuntos;
         })
         .catch(error => {
             console.error('Error:', error);
