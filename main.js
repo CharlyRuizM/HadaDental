b7f294daddfb16a5358557120e886236b7433d27
CarlosRuiz <ingecarlosrm@gmail.com>
2024-01-17 23:45:02 -0600

    [UPDATE] email v4 estilos y funciones nuevas

--- main.js
+++ main.js
@@ -1,23 +1,54 @@
-document.getElementById('searchButton').addEventListener('click', function() {
-    var userEmail = document.getElementById('userEmail').value;
-    
-    if (userEmail) {
-        var url = 'https://hooks.zapier.com/hooks/catch/12589050/3wnyhbi/?email=' + encodeURIComponent(userEmail);
+const searchForm = document.getElementById('searchForm');
+const loading = document.getElementById('loading');
+const result = document.getElementById('result');
 
-        fetch(url, {
+searchForm.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    var email = document.getElementById('emailInput').value;
+
+    // Mostrar la animación de carga
+    loading.style.display = 'block';
+
+    // Ocultar resultados anteriores
+    result.style.display = 'none';
+
+    // Utilizar CORS Anywhere como proxy
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    var targetUrl = 'https://northamerica-northeast1-hadadental.cloudfunctions.net/functionHadaDental';
+
+    try {
+        const response = await fetch(proxyUrl + targetUrl, {
             method: 'POST',
-            redirect: 'follow'
-        })
-        .then(response => response.json()) // Cambiado para manejar una respuesta JSON
-        .then(data => {
-            console.log(data); // Muestra los datos en la consola
-            document.getElementById('result').innerHTML = 'Nombre: ' + data.Nombre + ', Total Puntos: ' + data.totalPuntos;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            document.getElementById('result').innerHTML = 'Error al procesar la solicitud.';
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email: email }),
         });
-    } else {
-        document.getElementById('result').innerHTML = 'Por favor, introduce un correo electrónico válido.';
+
+        if (!response.ok) {
+            throw new Error('Respuesta de servidor no fue OK');
+        }
+
+        const data = await response.json();
+        
+        if (data.length === 0) {
+            // Mostrar mensaje de error cuando no se encuentran datos
+            result.innerHTML = 'No se encontraron datos para el correo proporcionado.';
+        } else {
+            // Mostrar los resultados cuando se encuentran datos
+            const resultHTML = data.map(user => `Nombre: ${user.Nombre}, Puntos Totales: ${user.PuntosTotales}`).join('<br>');
+            result.innerHTML = resultHTML;
+        }
+
+        // Ocultar la animación de carga después de recibir la respuesta
+        loading.style.display = 'none';
+        // Mostrar los resultados o mensaje de error
+        result.style.display = 'block';
+    } catch (error) {
+        console.error('Error:', error);
+        result.innerHTML = 'Error en la búsqueda';
+
+        // Ocultar la animación de carga en caso de error
+        loading.style.display = 'none';
     }
 });
